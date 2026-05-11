5ff942a7c8d820808240d4c3d46508621f5d7444
SFedora <s13flores18@gmail.com>
2026-05-10 21:48:59 -0400

    Ejercicio 10
    Este programa en Python implementa la generación de la sucesión de Fibonacci, un modelo matemático donde cada término es la suma de los dos anteriores, e integra la persistencia de datos mediante el manejo de archivos. El script utiliza un enfoque iterativo con asignación múltiple (a, b = b, a + b) para calcular los elementos de forma eficiente, almacenándolos en una lista que luego es volcada a un archivo físico.

---
+++ Python/Ejercicios_python/Ejercicio_10.py
@@ -0,0 +1,5 @@
+# 10. Sucesiones típicas
+
+# Haz un programa que muestre por pantalla los N primeros elementos de la sucesión de Fibonacci.
+
+# Mmmm, y ahora, escríbelos a un fichero llamado fibonacci.txt
