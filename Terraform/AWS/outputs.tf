ddd7c985273fd0e1eee977627bb197beb048729a
SFedora <s13flores18@gmail.com>
2026-06-24 00:58:39 -0400

    Limpiar detalles_red_servidores en outputs.tf para no mostrar IP publica en Server-2

--- Terraform/AWS/outputs.tf
+++ Terraform/AWS/outputs.tf
@@ -1,17 +1,16 @@
 # Bloque de salida (Output) para visualizar información útil en la terminal
 output "detalles_red_servidores" {
-  description = "IPs y nombres DNS asignados por AWS"
+  description = "Direcciones IP y DNS de la infraestructura desplegada en AWS"
 
   value = {
-    "Server-1" = {
-      ip_privada = aws_instance.vm_web.private_ip
-      ip_publica = aws_instance.vm_web.public_ip
-      dns        = aws_instance.vm_web.private_dns
+    "Server-1 (Web/API - Publico)" = {
+      ip_publica  = aws_instance.vm_web.public_ip
+      ip_privada  = aws_instance.vm_web.private_ip
+      dns_interno = aws_instance.vm_web.private_dns
     }
-    "Server-2" = {
-      ip_privada = aws_instance.vm_db.private_ip
-      ip_publica = aws_instance.vm_db.public_ip
-      dns        = aws_instance.vm_db.private_dns
+    "Server-2 (PostgreSQL - Privado)" = {
+      ip_privada   = aws_instance.vm_db.private_ip
+      dns_interno  = aws_instance.vm_db.private_dns
     }
   }
 }
